30551ccb24117a20db6b7f2431e76a8b8bfe3836
toast1023 <toast1023@users.noreply.github.com>
2024-02-10 16:22:46 +0000

    deploy: osmosys-ai/osmosys_frontend@fcffd95abdb7a2b411e529065e3697fe6ed4f522

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,28 @@
 import React from 'react'
 import styles from '../style';
 import { Billing,CardDeal,Business,Clients,CTA,Stats,Footer,Testimonials,Hero, About, Founders } from '../components/Home';
+import CookieConsent from "react-cookie-consent";
 
 
 const Home = () => {
   return (
+
     <div>
       <div className = {`bg-primary ${styles.flexStart}`}>
         <div className = {`${styles.boxWidth}`}>
           <Hero/>
         </div>
       </div> 
+      <CookieConsent
+        enableDeclineButton
+        flipButtons
+        onDecline={() => {}}
+        style={{background: "black",textAlign:"center", padding:"0.2em"}}
+        buttonStyle = {{background: "#2bfff8", borderRadius:"4px"}}
+        declineButtonStyle = {{borderRadius:"4px"}}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
 
       <div className = {`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className = {`${styles.boxWidth}`}>
